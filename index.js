891305e73970b5e9db665c61d61f06cc3ce5ab1a
papagei89 <10281319+papagei89@users.noreply.github.com>
2021-04-29 18:35:24 +0200

    init

---
+++ index.js
@@ -0,0 +1,75 @@
+//This code is hosted on this domain >>> https://i-want-to-join-ideta.herokuapp.com
+
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+const Joi = require('joi');
+
+const Ideta = "A software that automates conversations thanks to a great team !";
+
+const valid_candidacy = Joi.object({
+    first_name: Joi.string()
+        .alphanum()
+        .min(2)
+        .max(30)
+        .required(),
+    last_name: Joi.string()
+        .alphanum()
+        .min(2)
+        .max(30)
+        .required(),
+    visited_ideta_website: Joi.boolean().valid(true).required(),
+    email: Joi.string()
+        .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net', 'io'] } }).required(),
+    abilities: Joi.array().items(Joi.string()).required(),
+    extra_professional_activities: Joi.array().items(Joi.string()).required(),
+    location: Joi.string().required(),
+    what_is_ideta: Joi.string().valid(Ideta).required()
+})
+
+app.use(express.json());
+
+app.post('/apply', (req, res) => {
+	const candidate = req.body;
+	try {
+		console.log('Let\'s see who tried what : ', candidate, !valid_candidacy.validate(candidate).error);
+		if (!valid_candidacy.validate(candidate).error) {
+			console.log(candidate.abilities);
+			console.log(candidate.abilities.includes('Angular 2+'), candidate.abilities.includes('SCSS'), candidate.abilities.includes('HTML'));
+			if (candidate.abilities.includes('Angular 2+') &&
+				candidate.abilities.includes('SCSS') &&
+				candidate.abilities.includes('HTML')) {
+				if (candidate.location === 'Paris' ||
+					candidate.location === 'Remote') {
+					if (candidate.extra_professional_activities.includes('footbAll') ||
+						candidate.extra_professional_activities.includes('Supporting Barcelona')) {
+						res.send('You are a great fit to this company. \nSend me a mail at : ' +
+							(process.env.HE_DID_NOT_CHEAT_FOOTBALL || "yanis+whatisthepasscode@ideta.io"));
+					} else {
+						res.send('Have you ever considered playing football or supporting Barcelona ;-) ?\n' +
+							'Send me a mail at : ' +
+							(process.env.HE_DID_NOT_CHEAT_BUT_NO_FOOTBALL ||
+								"yanis+whatisthepasscode@ideta.io"));
+					}
+				} else {
+					res.send('Are you sure you want to apply ? \n' +
+						'Send me an email at :' + ("yanis+whatisthepasscode@ideta.io" || process.env.NOT_PARIS_NOT_REMOTE));
+				}
+			} else {
+				res.send('Even though this is some backend code, we are looking for a frontend developer !\n' +
+					'If you really want to work with us, send us a mail at contact@ideta.io.\n' +
+					'We will have other open positions soon !');
+			}
+		} else {
+			res.send('Thanks for trying to apply ! But your candidacy does not match our requirements.\n' +
+				'Check our requirements and try again :-) ');
+		}
+	} catch(error) {
+		res.send("What they tried to crash the server and make me look like a fool and get extra attention... : ", error);
+	}
+
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
